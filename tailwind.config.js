03c92e9d01ca894b8af0ac5b35e6a15f74f98209
abhijay rane <94125959+abjR265@users.noreply.github.com>
2025-05-01 00:25:49 -0400

    Update final tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-<<<<<<< HEAD
-      "./src/**/*.{js,ts,jsx,tsx}"
-=======
-      "./src/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> 766adf8550d8c04ccf7028a83b7042e25852c96e
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-<<<<<<< HEAD
-  
-=======
-  
->>>>>>> 766adf8550d8c04ccf7028a83b7042e25852c96e
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
