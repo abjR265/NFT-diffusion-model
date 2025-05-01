fd58110052ac59db77b8b0d7fd112c1b647d93ee
abhijay rane <94125959+abjR265@users.noreply.github.com>
2025-04-30 23:12:32 -0400

    Fix: resolve merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-const nextConfig = {
-  // leave empty unless adding real config
-=======
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
->>>>>>> 766adf8550d8c04ccf7028a83b7042e25852c96e
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
